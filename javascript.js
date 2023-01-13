eb3e86b320d145f9b39df48a6fa2d353558f9cda
russellella <116303658+russellella@users.noreply.github.com>
2023-01-12 19:04:02 -0800

    Update functions and event listeners
    -Updated the operate function to include if/else for operations
    -Updated op varialbe when one of the operate buttons is pressed (+-*/)
    -Run operate when equals is clicked

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
+// Operating Functions
 function add(a,b) {
     let c = a + b;
     document.getElementById("calcdisplay").innerText = c;
-    return c;
+    console.log(c);
 }
 
 function sub(a,b) {
@@ -27,15 +28,50 @@ function div(a,b) {
     }
 }
 
-function operate(a, op, b) {
-    op(a,b);
-}
-let dispValue;
 
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
+// Update display with button push
+const numbuttons = document.querySelectorAll(".numbutton");
+numbuttons.forEach((button) => {
     button.addEventListener("click", () => {
         document.getElementById("calcdisplay").innerText = button.id;
-        let dispValue = button.id;
+        let a = button.id;
+        console.log(a);
     });
-});
+})
+
+// When +-*/ pressed, let op = button id
+const calbuttons = document.querySelectorAll(".calbutton");
+calbuttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let op = button.id;
+        console.log(op);
+    });
+})
+
+
+let a = 2;
+let b = 3;
+
+
+function operate(a, op, b) {
+    if (op = "add") {
+        add(a,b);
+        console.log("operate ran");
+    } else if (op = "sub") {
+        sub(a,b);
+    }   else if (op = "mul") {
+        mul(a,b);
+    } else if (op = "div") {
+        div(a,b);
+    }
+}
+
+
+// When = pressed, run operate function
+
+document.getElementById("equ").onclick = function() {operate()};
+
+
+
+// Test operate function:
+
