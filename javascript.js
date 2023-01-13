a04d457d3fe44464993d7393fbe0a6f5ede0ebbe
russellella <116303658+russellella@users.noreply.github.com>
2023-01-13 09:48:37 -0800

    Fix bug with onclick updating variable
    onclick functions for numbers & operators now correctly update variables

--- javascript.js
+++ javascript.js
@@ -1,22 +1,33 @@
-// Testing Variables
-let a = 3;
-let b = 2;
+// When +-*/ pressed, let op = button.id
+let op;
 
-// Operating Functions
+const opbuttons = document.querySelectorAll(".opbutton");
+opbuttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        op = button.id;
+    });
+})
+
+// TESTING
+
+let y = 2;
+let c;
+
+// add/sub/mul/div Functions
 function add() {
-    let c = a + b;
+    c = a + b;
     document.getElementById("calcdisplay").innerText = c;
     console.log(c);
 }
 
 function sub() {
-    let c = a - b;
+    c = a - b;
     document.getElementById("calcdisplay").innerText = c;
     return c;
 }
 
 function mul() {
-    let c = a * b;
+    c = a * b;
     document.getElementById("calcdisplay").innerText = c;
     return c;
 }
@@ -26,46 +37,24 @@ function div() {
         document.getElementById("calcdisplay").innerText = "Warning! Black hole imminent!";
         return "Warning! Black hole imminent!"
     } else {
-        let c = a/b;
+        c = a/b;
         document.getElementById("calcdisplay").innerText = c;
         return c;
     }
 }
 
-
-// Update display with button push - TEMP DISABLED FOR TESTING
-/*
-const numbuttons = document.querySelectorAll(".numbutton");
-numbuttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        document.getElementById("calcdisplay").innerText = button.id;
-        let a = button.id;
-        console.log(a);
-    });
-})
-*/
-
-// When +-*/ pressed, let op = button id - WORKS
-const calbuttons = document.querySelectorAll(".calbutton");
-calbuttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        let op = button.id;
-        console.log(op);
-    });
-})
-
 // Operate function
-function operate() {
-    if (op = "add") {
+function operate(op) {
+    if (op == "add") {
         add();
         console.log("+ operate ran");
-    } else if (op = "sub") {
+    } else if (op == "sub") {
         sub();
         console.log("- operate ran");
-    }   else if (op = "mul") {
+    }   else if (op == "mul") {
         mul();
         console.log("* operate ran");
-    } else if (op = "div") {
+    } else if (op == "div") {
         div();
         console.log("/ operate ran");
     } else (
@@ -73,4 +62,16 @@ function operate() {
     )
 }
 
+// Equals runs operate function
 document.getElementById("equ").onclick = function() {operate()};
+
+// Update display with button push - Update display works, but assigning button.id to a does not
+const numbuttons = document.querySelectorAll(".numbutton");
+numbuttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        document.getElementById("calcdisplay").innerText = button.id;
+        a = +button.id;
+    });
+})
+
+
